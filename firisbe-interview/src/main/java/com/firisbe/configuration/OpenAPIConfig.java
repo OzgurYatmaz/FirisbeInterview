418795e1f7ff79467327b575aee6bf245119beb0
unknown <ozguryatmaz@gmail.com>
2024-05-26 00:53:41 +0300

    Docker support

--- firisbe-interview/src/main/java/com/firisbe/configuration/OpenAPIConfig.java
+++ firisbe-interview/src/main/java/com/firisbe/configuration/OpenAPIConfig.java
@@ -3,9 +3,6 @@
  */
 package com.firisbe.configuration;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +10,6 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.servers.Server;
 
 /**
  * Swagger configuration is made from here.
@@ -31,11 +27,7 @@ import io.swagger.v3.oas.models.servers.Server;
 @Configuration
 public class OpenAPIConfig {
 
-	@Value("${openapi.dev-url}")
-	private String devUrl;
-
-	@Value("${openapi.prod-url}")
-	private String prodUrl;
+ 
 
 	/**
 	 * 
@@ -47,14 +39,7 @@ public class OpenAPIConfig {
 	 */
 	@Bean
 	public OpenAPI myOpenAPI() {
-		Server devServer = new Server();
-		devServer.setUrl(devUrl);
-		devServer.setDescription("Server URL in Development environment");
-
-		Server prodServer = new Server();
-		prodServer.setUrl(prodUrl);
-		prodServer.setDescription("Server URL in Production environment");
-
+		 
 		Contact contact = new Contact();
 		contact.setEmail("ozguryatmaz@yandex.com");
 		contact.setName("Ozgur Yatmaz");
@@ -75,6 +60,6 @@ public class OpenAPIConfig {
 						+ "     <br /> Build Tool: Maven")
 				.license(mitLicense);
 
-		return new OpenAPI().info(info).servers(List.of(devServer, prodServer));
+		return new OpenAPI().info(info);
 	}
 }
